9591722ec502df77826d9801a08c61d9e98a97c4
Adam <almcchesney@gmail.com>
2019-01-20 17:17:15 -0600

    Adding links

--- snowcli/main.py
+++ snowcli/main.py
@@ -42,6 +42,7 @@ def __get_connection():
 @click.argument('table')
 @click.command()
 def main(table, action, param, custom):
+    ''' Documentation: https://amacc.github.io/SNowCli/ '''
     param = __convert_to_dict(param)
 
     try:
